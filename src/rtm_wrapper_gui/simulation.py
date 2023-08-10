fe9e0e641a6116bcf7be11b41e02ddb7d5788c19
Brian Schubert <schubert.b@northeastern.edu>
2023-08-09 21:38:13 -0400

    Add generalized simulation loading panel

---
+++ src/rtm_wrapper_gui/simulation.py
@@ -0,0 +1,117 @@
+"""
+GUI elements for running simulations and loading simulation results.
+"""
+
+from __future__ import annotations
+
+import logging
+import pathlib
+
+import xarray as xr
+from PySide6 import QtCore, QtGui, QtWidgets
+
+from rtm_wrapper_gui import util
+
+
+class SimulationPanel(QtWidgets.QWidget):
+    sim_producers: SimulationProducerTabs
+
+    summary: ResultsSummary
+
+    def __init__(self, parent: QtWidgets.QWidget | None = None) -> None:
+        super().__init__(parent)
+
+        layout = QtWidgets.QVBoxLayout()
+        self.setLayout(layout)
+
+        self.sim_producers = SimulationProducerTabs()
+        layout.addWidget(self.sim_producers)
+
+        self.summary = ResultsSummary()
+        layout.addWidget(self.summary)
+
+        self.sim_producers.new_results.connect(self.summary.summarize_results)
+
+
+class SimulationProducer(QtWidgets.QWidget):
+    new_results = QtCore.Signal(util.RtmResults)
+
+
+class SimulationProducerTabs(SimulationProducer):
+    tabs: QtWidgets.QTabWidget
+
+    def __init__(self, parent: QtWidgets.QWidget | None = None) -> None:
+        super().__init__(parent)
+        layout = QtWidgets.QVBoxLayout()
+        self.setLayout(layout)
+
+        self.tabs = QtWidgets.QTabWidget()
+        layout.addWidget(self.tabs)
+
+        self.tabs.addTab(
+            FileSimulationProducer(),
+            self.style().standardIcon(QtWidgets.QStyle.StandardPixmap.SP_FileIcon),
+            "File",
+        )
+
+        for idx in range(self.tabs.count()):
+            self.tabs.widget(idx).new_results.connect(self.new_results)
+
+
+class FileSimulationProducer(SimulationProducer):
+    browse_button: QtWidgets.QPushButton
+
+    def __init__(self, parent: QtWidgets.QWidget | None = None) -> None:
+        super().__init__(parent)
+        layout = QtWidgets.QVBoxLayout()
+        self.setLayout(layout)
+
+        self.browse_button = QtWidgets.QPushButton()
+        self.browse_button.setText("Select results file")
+        layout.addWidget(self.browse_button)
+
+        self.browse_button.clicked.connect(self._on_browse_button_clicked)
+
+    @QtCore.Slot()
+    def _on_browse_button_clicked(self) -> None:
+        logger = logging.getLogger(__name__)
+        dialog = QtWidgets.QFileDialog()
+
+        selected_file, _selected_filter = dialog.getOpenFileName(
+            None,
+            "Select results file",
+            str(pathlib.Path.cwd()),
+            "netCDF File (*.nc);;Any File (*)",
+        )
+        if selected_file == "":
+            # Dialog was closed / cancelled.
+            logger.debug("file selection cancelled")
+            return
+
+        try:
+            dataset = xr.open_dataset(selected_file)
+        except Exception as ex:
+            logger.error("failed to load dataset", exc_info=ex)
+            return
+        logger.debug("loaded dataset\n%r", dataset)
+
+        self.new_results.emit(util.RtmResults(dataset))
+
+
+class ResultsSummary(QtWidgets.QWidget):
+    temp_textedit: QtWidgets.QTextEdit
+
+    def __init__(self, parent: QtWidgets.QWidget | None = None) -> None:
+        super().__init__(parent)
+        layout = QtWidgets.QVBoxLayout()
+        self.setLayout(layout)
+
+        self.temp_textedit = QtWidgets.QTextEdit()
+        self.temp_textedit.setText("No simulation results loaded.")
+        self.temp_textedit.setLineWrapMode(QtWidgets.QTextEdit.LineWrapMode.NoWrap)
+        self.temp_textedit.setFont(QtGui.QFont("monospace"))
+        layout.addWidget(self.temp_textedit)
+
+    @QtCore.Slot(util.RtmResults)
+    def summarize_results(self, results: util.RtmResults) -> None:
+        self.temp_textedit.setText(repr(results))
--- src/rtm_wrapper_gui/window.py
+++ src/rtm_wrapper_gui/window.py
@@ -1,14 +1,13 @@
-import logging
-import pathlib
 from typing import Any
 
 import xarray as xr
-from PySide6 import QtGui, QtWidgets
+from PySide6 import QtWidgets
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QIcon
 
 from rtm_wrapper_gui import util
 from rtm_wrapper_gui.plot import FigureWidget
+from rtm_wrapper_gui.simulation import SimulationPanel
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -32,7 +31,7 @@ class MainWindow(QtWidgets.QMainWindow):
         version_info = util.make_detailed_version(util.DISTRIBUTION_NAME).replace(
             " (", "\n("
         )
-        self.plots_widget.figure_widget.show_splash(
+        self.figure_widget.show_splash(
             version_info,
             horizontalalignment="center",
             color="grey",
@@ -46,7 +45,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def _init_signals(self) -> None:
         self.plot_button.clicked.connect(self.figure_widget.draw)
-        self.browse_button.clicked.connect(self._on_browse)
+        # self.browse_button.clicked.connect(self._on_browse)
 
     def _init_central_widget(self) -> None:
         # Setup central widget and layout.
@@ -57,7 +56,8 @@ class MainWindow(QtWidgets.QMainWindow):
 
         # Add main vertical splitter.
         top_splitter = QtWidgets.QSplitter(Qt.Orientation.Horizontal)
-        top_splitter.addWidget(self._init_data_widget())
+        top_splitter.addWidget(SimulationPanel(self))
+        # top_splitter.addWidget(self._init_data_widget())
         top_splitter.addWidget(self._init_plot_widget())
         top_splitter.setHandleWidth(10)
         top_layout.addWidget(top_splitter)
@@ -74,22 +74,22 @@ class MainWindow(QtWidgets.QMainWindow):
             """
         )
 
-    def _init_data_widget(self) -> QtWidgets.QWidget:
-        frame_widget = QtWidgets.QWidget()
-        frame_layout = QtWidgets.QVBoxLayout()
-        frame_widget.setLayout(frame_layout)
-
-        self.browse_button = QtWidgets.QPushButton()
-        self.browse_button.setText("Select results file")
-        frame_layout.addWidget(self.browse_button)
-
-        self.dataset_textedit = QtWidgets.QTextEdit()
-        self.dataset_textedit.setText("No simulation results loaded.")
-        self.dataset_textedit.setLineWrapMode(QtWidgets.QTextEdit.LineWrapMode.NoWrap)
-        self.dataset_textedit.setFont(QtGui.QFont("monospace"))
-        frame_layout.addWidget(self.dataset_textedit)
-
-        return frame_widget
+    # def _init_data_widget(self) -> QtWidgets.QWidget:
+    #     frame_widget = QtWidgets.QWidget()
+    #     frame_layout = QtWidgets.QVBoxLayout()
+    #     frame_widget.setLayout(frame_layout)
+    #
+    #     self.browse_button = QtWidgets.QPushButton()
+    #     self.browse_button.setText("Select results file")
+    #     frame_layout.addWidget(self.browse_button)
+    #
+    #     self.dataset_textedit = QtWidgets.QTextEdit()
+    #     self.dataset_textedit.setText("No simulation results loaded.")
+    #     self.dataset_textedit.setLineWrapMode(QtWidgets.QTextEdit.LineWrapMode.NoWrap)
+    #     self.dataset_textedit.setFont(QtGui.QFont("monospace"))
+    #     frame_layout.addWidget(self.dataset_textedit)
+    #
+    #     return frame_widget
 
     def _init_plot_widget(self) -> QtWidgets.QWidget:
         frame_widget = QtWidgets.QWidget()
@@ -114,25 +114,25 @@ class MainWindow(QtWidgets.QMainWindow):
 
         return frame_widget
 
-    def _on_browse(self) -> None:
-        logger = logging.getLogger(__name__)
-        dialog = QtWidgets.QFileDialog()
-
-        selected_file, _selected_filter = dialog.getOpenFileName(
-            None,
-            "Select results file",
-            str(pathlib.Path.cwd()),
-            "netCDF File (*.nc);;Any File (*)",
-        )
-        if selected_file == "":
-            # Dialog was closed / cancelled.
-            logger.debug("file selection cancelled")
-            return
-
-        try:
-            self._dataset = xr.open_dataset(selected_file)
-        except Exception as ex:
-            logger.error("failed to load dataset", exc_info=ex)
-            return
-        logger.debug("loaded dataset\n%r", self._dataset)
-        self.dataset_textedit.setText(repr(self._dataset))
+    # def _on_browse(self) -> None:
+    #     logger = logging.getLogger(__name__)
+    #     dialog = QtWidgets.QFileDialog()
+    #
+    #     selected_file, _selected_filter = dialog.getOpenFileName(
+    #         None,
+    #         "Select results file",
+    #         str(pathlib.Path.cwd()),
+    #         "netCDF File (*.nc);;Any File (*)",
+    #     )
+    #     if selected_file == "":
+    #         # Dialog was closed / cancelled.
+    #         logger.debug("file selection cancelled")
+    #         return
+    #
+    #     try:
+    #         self._dataset = xr.open_dataset(selected_file)
+    #     except Exception as ex:
+    #         logger.error("failed to load dataset", exc_info=ex)
+    #         return
+    #     logger.debug("loaded dataset\n%r", self._dataset)
+    #     self.dataset_textedit.setText(repr(self._dataset))
