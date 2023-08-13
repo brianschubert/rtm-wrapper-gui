4b7a69f10636844c905625ebcb049d63d0a42fcc
Brian Schubert <schubert.b@northeastern.edu>
2023-08-13 14:14:44 -0400

    Add minimal working plotting config widget connections

--- src/rtm_wrapper_gui/plot/widgets.py
+++ src/rtm_wrapper_gui/plot/widgets.py
@@ -8,12 +8,14 @@ import logging
 from typing import TYPE_CHECKING, Any, ClassVar, Iterator, Optional
 
 import numpy as np
+import xarray as xr
 from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg, NavigationToolbar2QT
 from matplotlib.figure import Figure
 from PySide6 import QtCore, QtWidgets
 
 from rtm_wrapper_gui import util
-from rtm_wrapper_gui.plot.plotters import DatasetPlotter
+
+from . import plotters
 
 if TYPE_CHECKING:
     from matplotlib.axes import Axes
@@ -131,16 +133,111 @@ class RtmResultsPlots(QtWidgets.QWidget):
         self.controls = PlotControls(self)
         layout.addWidget(self.controls)
 
+        self._init_signals()
+
+    def _init_signals(self) -> None:
+        self.controls.plot_button.clicked.connect(self._on_plot_clicked)
+        self.active_results.value_changed[object].connect(self._on_results_changed)
+
+    @QtCore.Slot()
+    def _on_plot_clicked(self) -> None:
+        logger = logging.getLogger(__name__)
+        logger.debug("plot button clicked")
+
+        if self.active_results.value is None:
+            QtWidgets.QMessageBox.warning(
+                self,
+                "Misconfigured plotter",
+                f"Cannot create plot: no simulation results are loaded",
+            )
+            return
+
+        plotter_index = self.controls.plotter_selector.currentIndex()
+        plotter_config: DatasetPlotterConfigWidget = (  # type: ignore
+            self.controls.plotter_controls.widget(plotter_index)
+        )
+        if plotter_config is None:
+            QtWidgets.QMessageBox.warning(
+                self,
+                "Misconfigured plotter",
+                f"Cannot create plot: no plotter selected",
+            )
+            logger.warning("plot button clicked, but no plotter is active")
+            return
+
+        try:
+            plotter = plotter_config.make_plotter()
+        except Exception as ex:
+            logger.warning("exception raised during plotter creation", exc_info=ex)
+            QtWidgets.QMessageBox.warning(
+                self,
+                "Misconfigured plotter",
+                f"Cannot create plot with current configuration: {ex}",
+            )
+            return
+
+        logger.debug("plotting")
+        self.figure_widget.wipe_axes()
+        plotter.plot(
+            self.figure_widget.canvas.figure, self.active_results.value.dataset
+        )
+        self.figure_widget._refresh_canvas()
+
+    def _on_results_changed(self, new_results: util.RtmResults | None) -> None:
+        logger = logging.getLogger(__name__)
+
+        if new_results is None:
+            logger.debug("results is None - disabling all plotters")
+            self.controls.plotter_selector.setCurrentIndex(-1)
+            for plotter_idx in range(0, self.controls.plotter_controls.count()):
+                plotter_combo_item = self.controls.plotter_selector.model().item(
+                    plotter_idx, 0
+                )
+                plotter_combo_item.setEnabled(False)
+            return
+
+        for plotter_idx in range(self.controls.plotter_controls.count()):
+            plotter: DummyPlotterConfig = self.controls.plotter_controls.widget(plotter_idx)  # type: ignore
+            plotter.setup_for_dataset(new_results.dataset)
+
+            plotter_combo_item = self.controls.plotter_selector.model().item(
+                plotter_idx, 0
+            )
+            enabled = plotter.can_plot_dataset(new_results.dataset)
+            plotter_combo_item.setEnabled(enabled)
+            logger.debug(
+                f"plotter %d %s (%s) enabled: %s",
+                plotter_idx,
+                plotter.display_name,
+                type(plotter).__name__,
+                enabled,
+            )
+
+        current_index = self.controls.plotter_selector.currentIndex()
+        active_plotter = self.controls.plotter_selector.model().item(current_index, 0)
+        if active_plotter is not None and not active_plotter.isEnabled():
+            logger.debug("active plotter was disabled - resetting to no active plotter")
+            self.controls.plotter_selector.setCurrentIndex(-1)
+            self.controls.plotter_controls.setCurrentIndex(-1)
+
 
 class DatasetPlotterConfigWidget(QtWidgets.QWidget):
-    plotter: DatasetPlotter
+    def __init__(self, parent: QtWidgets.QWidget | None = None) -> None:
+        super().__init__(parent)
+        self.setLayout(QtWidgets.QHBoxLayout())
 
-    def extract_config(self) -> dict[str, Any]:
-        return {}
+    def make_plotter(self) -> plotters.DatasetPlotter:
+        raise NotImplementedError
 
     @property
     def display_name(self) -> str:
         return self.__class__.__name__
+
+    def can_plot_dataset(self, dataset: xr.Dataset) -> bool:
+        return False
+
+    def setup_for_dataset(self, dataset: xr.Dataset) -> None:
+        ...
 
 
 class PlotterRegistry:
@@ -182,9 +279,11 @@ class PlotControls(QtWidgets.QWidget):
         layout.addLayout(left_controls)
 
         self.plotter_controls = QtWidgets.QStackedWidget()
+        self.plotter_controls.hide()  # start hidden
         layout.addWidget(self.plotter_controls)
 
         self.plotter_selector = QtWidgets.QComboBox()
+        self.plotter_selector.setPlaceholderText("<no selection>")
         left_controls.addWidget(self.plotter_selector)
 
         for plotter_cls in self.plotters:
@@ -217,26 +316,92 @@ class PlotControls(QtWidgets.QWidget):
         self._init_signals()
 
     def _init_signals(self) -> None:
-        self.plot_button.clicked.connect(self._on_plot_clicked)
-        self.plotter_selector.activated[int].connect(
-            self.plotter_controls.setCurrentIndex
-        )
+        self.plotter_selector.currentIndexChanged[int].connect(self._on_plotter_changed)
 
-    @QtCore.Slot()
-    def _on_plot_clicked(self) -> None:
+    def _on_plotter_changed(self, index: int) -> None:
         logger = logging.getLogger(__name__)
-        logger.debug("plot button clicked")
+        logger.debug("plotter controls index changed to %r", index)
+        self.plotter_controls.setCurrentIndex(index)
+        if index == -1:
+            self.plotter_controls.hide()
+        else:
+            self.plotter_controls.show()
 
 
-@PlotControls.plotters.register
-class DummyPlotterConfig(DatasetPlotterConfigWidget):
-    textedit: QtWidgets.QTextEdit
+class FixedDimVariablePlotter(DatasetPlotterConfigWidget):
+    required_dims: list[str]
+
+    variable_selector: _LabelledListWidget
+
+    dim_lists: list[_LabelledListWidget]
 
     def __init__(self, parent: QtWidgets.QWidget | None = None) -> None:
         super().__init__(parent)
-        layout = QtWidgets.QHBoxLayout()
-        self.setLayout(layout)
+        self.variable_selector = _LabelledListWidget("Variable")
+        self.layout().addWidget(self.variable_selector)
+        self.dim_lists = []
 
-        self.textedit = QtWidgets.QTextEdit()
-        self.textedit.setText("test")
-        layout.addWidget(self.textedit)
+    def setup_for_dataset(self, dataset: xr.Dataset) -> None:
+        super().setup_for_dataset(dataset)
+
+        self.variable_selector.list.clear()
+        for var_name in dataset.data_vars.keys():
+            self.variable_selector.list.addItem(var_name)
+
+        for widget in self.dim_lists:
+            self.layout().removeWidget(widget)
+            # widget.deleteLater()
+
+        for plot_dim in self.required_dims:
+            list_widget = _LabelledListWidget(plot_dim)
+            self.dim_lists.append(list_widget)
+            for data_dim in dataset.indexes.dims.keys():
+                list_widget.list.addItem(f"{data_dim}")
+            self.layout().addWidget(list_widget)
+
+    def can_plot_dataset(self, dataset: xr.Dataset) -> bool:
+        return len(dataset.indexes.dims) == len(self.required_dims)
+
+
+@PlotControls.plotters.register
+class SingleSweepVariablePlotter(FixedDimVariablePlotter):
+    required_dims: tuple[str, ...] = ("x-axis",)
+
+    @property
+    def display_name(self) -> str:
+        return "Single Sweep"
+
+    def _get_config(self) -> dict[str, Any]:
+        try:
+            variable = self.variable_selector.list.currentItem().text()
+        except AttributeError:
+            raise RuntimeError("variable not selected")
+
+        dims = []
+        for req_dim, selector in zip(self.required_dims, self.dim_lists):
+            try:
+                dims.append(selector.list.currentItem().text())
+            except AttributeError:
+                raise RuntimeError(f"no dimension selected for {req_dim}")
+
+        return {"variable": variable, "dims": dims}
+
+    def make_plotter(self) -> plotters.DatasetPlotter:
+        return plotters.SingleSweepVariablePlotter(**self._get_config())
+
+
+class _LabelledListWidget(QtWidgets.QWidget):
+    label: QtWidgets.QLabel
+
+    list: QtWidgets.QListWidget
+
+    def __init__(self, label: str, parent: QtWidgets.QWidget | None = None) -> None:
+        super().__init__(parent)
+        self.setLayout(QtWidgets.QVBoxLayout())
+
+        self.label = QtWidgets.QLabel()
+        self.label.setText(label)
+        self.layout().addWidget(self.label)
+
+        self.list = QtWidgets.QListWidget()
+        self.layout().addWidget(self.list)
